539198f676641eff4ed0c61d3a59358dbf4c7e19
vitoria <vitoria.le@hotmail.com>
2024-06-06 17:30:15 -0300

    conta banco  no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){ 
+        
+    int numero = Integer.parseInt(args[0]);
+    String agencia = args[1];
+    String nomeCliente = args[2] + " " + args[3];
+    double saldo = 237.48;
+
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+
     }
+
 }
